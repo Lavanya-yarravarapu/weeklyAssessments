90a49d81bea40fdbc64b773fb7226fb0f99360e5
Lavanya-yarravarapu <lavanya.msa@gmail.com>
2024-06-14 13:51:06 +0530

    week2 03

---
+++ week2_date_03_05_2024/src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,33 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    public LoginServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().append("Served at: ").append(request.getContextPath());
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if ("admin".equals(username) && "password".equals(password)) {
+            request.setAttribute("username", username);
+            request.getRequestDispatcher("welcome.jsp").forward(request, response);
+        } else {
+            response.getWriter().println("Invalid credentials");
+        }
+    }
+}

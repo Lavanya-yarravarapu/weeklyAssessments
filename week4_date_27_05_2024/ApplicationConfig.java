5b94fee11b816aa3f400d4eac9911a4935cd8b9f
Lavanya-yarravarapu <lavanya.msa@gmail.com>
2024-06-14 19:03:31 +0530

    week4_date_27_05_2024

---
+++ week4_date_27_05_2024/ApplicationConfig.java
@@ -0,0 +1,19 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "com.example")
+@EnableWebMvc
+public class ApplicationConfig implements WebMvcConfigurer {
+    @Bean
+    public GreetingController greetingController() {
+        return new GreetingController();
+    }
+}
+
